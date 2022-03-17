5f9e15881fe3032d13f1cded7e23c0bb849e0acb
Amit Chavda <amit.c@simformsolutions.com>
2022-03-17 11:08:31 +0530

    Custom Access denied page added

---
+++ src/main/java/com/springbootapp/emlpoyee/helper/CustomAccessDeniedHandler.java
@@ -0,0 +1,34 @@
+package com.springbootapp.emlpoyee.helper;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import com.springbootapp.emlpoyee.aspects.GeneralLoggingAspect;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+	final private Logger LOG = LoggerFactory.getLogger(GeneralLoggingAspect.class);
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+			AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			LOG.warn("User : " + auth.getName() + " attempted to access the protected URL: " + request.getRequestURI());
+		}
+		String msg = "You don't have permission to access this URL!";
+		response.sendRedirect(request.getContextPath() + "/AccessDenied");
+	}
+
+}
